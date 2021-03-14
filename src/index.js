198bd2ba8191cb4d1b235d8c2bb1fe4395ad2aef
alekse1f <67468043+alekse1f@users.noreply.github.com>
2021-03-14 20:08:11 +0300

    feat: add fuction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(af, qt) {
+    const arr = af.split("");
+    let st = [];
+    console.log(arr, st);
+    for (let i = 0; i < arr.length; i++) {
+        const symb = qt.indexOf(arr[i]); // 0
+        console.log(i, symb);
+        if (symb % 2 === 0) {
+            if (arr[i] == qt[symb + 1]) {
+                // | == |
+                st[st.length - 1] == symb + 1 ? st.pop() : st.push(symb + 1);
+            } else {
+                st.push(symb);
+            }
+        } else {
+            if (st.length == 0 || st[st.length - 1] != symb - 1) {
+                return false;
+            } else {
+                st.pop();
+            }
+        }
+    }
+    return st.length == 0;
 }
